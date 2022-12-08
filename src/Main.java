5af8bcfe2689adfb0e121e2437d9569dd3cb117d
emj4096 <emj4096@proton.me>
2022-12-07 21:08:53 -0700

    small cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Scratch {
-
-    public static void main(String[] args) {
-
-        final String[][] capitalsStates = {
-                {"Montgomery", "Alabama"},
-                {"Juneau", "Alaska"},
-                {"Phoenix", "Arizona"},
-                {"Little Rock", "Arkansas"},
-                {"Sacramento", "California"},
-                {"Denver", "Colorado"},
-                {"Hartford", "Connecticut"},
-                {"Dover", "Delaware"},
-                {"Tallahassee", "Florida"},
-                {"Atlanta", "Georgia"},
-                {"Honolulu", "Hawaii"},
-                {"Boise", "Idaho"},
-                {"Springfield", "Illinois"},
-                {"Indianapolis", "Indiana"},
-                {"Des Moines", "Iowa"},
-                {"Topeka", "Kansas"},
-                {"Frankfort", "Kentucky"},
-                {"Baton Rouge", "Louisiana"},
-                {"Augusta", "Maine"},
-                {"Annapolis", "Maryland"},
-                {"Boston", "Massachusetts"},
-                {"Lansing", "Michigan"},
-                {"St. Paul", "Minnesota"},
-                {"Jackson", "Mississippi"},
-                {"Jefferson City", "Missouri"},
-                {"Helena", "Montana"},
-                {"Lincoln", "Nebraska"},
-                {"Carson City", "Nevada"},
-                {"Concord", "New Hampshire"},
-                {"Trenton", "New Jersey"},
-                {"Santa Fe", "New Mexico"},
-                {"Albany", "New York"},
-                {"Raleigh", "North Carolina"},
-                {"Bismarck", "North Dakota"},
-                {"Columbus", "Ohio"},
-                {"Oklahoma City", "Oklahoma"},
-                {"Salem", "Oregon"},
-                {"Harrisburg", "Pennsylvania"},
-                {"Providence", "Rhode Island"},
-                {"Columbia", "South Carolina"},
-                {"Pierre", "South Dakota"},
-                {"Nashville", "Tennessee"},
-                {"Austin", "Texas"},
-                {"Salt Lake City", "Utah"},
-                {"Montpelier", "Vermont"},
-                {"Richmond", "Virginia"},
-                {"Olympia", "Washington"},
-                {"Charleston", "West Virginia"},
-                {"Madison", "Wisconsin"},
-                {"Cheyenne", "Wyoming"},
-        };
-
-        // Print contents of array (line by line)
-        for (String[] capitalState : capitalsStates) {
-            System.out.println(Arrays.toString(capitalState));
-        }
-
-        // Bubble sort
-        CapitalStateArray csa = new CapitalStateArray(capitalsStates);
-        csa.bubbleSort();
-        // Begin guesses from user and output results at the end
-        GuessGame gg = new GuessGame(capitalsStates);
-        gg.play();
-
-        // Convert 2D array to hashmap
-
-        // Display hashmap
-
-        // Sort map using with TreeMap class
-
-        // Begin user state/capital response
-
-
-    }
-}
-
 class CapitalStateArray {
 
     String[][] strArray;
@@ -89,7 +8,6 @@ class CapitalStateArray {
     public CapitalStateArray(String[][] strArray) {
         this.strArray = strArray;
     }
-
 
     public void bubbleSort() {
         CapitalStateArray sorted;
@@ -153,4 +71,87 @@ class GuessGame {
                 "and %d incorrectly.%n", correct, wrong);
     }
 
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final String[][] capitalsStates = {
+                {"Montgomery", "Alabama"},
+                {"Juneau", "Alaska"},
+                {"Phoenix", "Arizona"},
+                {"Little Rock", "Arkansas"},
+                {"Sacramento", "California"},
+                {"Denver", "Colorado"},
+                {"Hartford", "Connecticut"},
+                {"Dover", "Delaware"},
+                {"Tallahassee", "Florida"},
+                {"Atlanta", "Georgia"},
+                {"Honolulu", "Hawaii"},
+                {"Boise", "Idaho"},
+                {"Springfield", "Illinois"},
+                {"Indianapolis", "Indiana"},
+                {"Des Moines", "Iowa"},
+                {"Topeka", "Kansas"},
+                {"Frankfort", "Kentucky"},
+                {"Baton Rouge", "Louisiana"},
+                {"Augusta", "Maine"},
+                {"Annapolis", "Maryland"},
+                {"Boston", "Massachusetts"},
+                {"Lansing", "Michigan"},
+                {"St. Paul", "Minnesota"},
+                {"Jackson", "Mississippi"},
+                {"Jefferson City", "Missouri"},
+                {"Helena", "Montana"},
+                {"Lincoln", "Nebraska"},
+                {"Carson City", "Nevada"},
+                {"Concord", "New Hampshire"},
+                {"Trenton", "New Jersey"},
+                {"Santa Fe", "New Mexico"},
+                {"Albany", "New York"},
+                {"Raleigh", "North Carolina"},
+                {"Bismarck", "North Dakota"},
+                {"Columbus", "Ohio"},
+                {"Oklahoma City", "Oklahoma"},
+                {"Salem", "Oregon"},
+                {"Harrisburg", "Pennsylvania"},
+                {"Providence", "Rhode Island"},
+                {"Columbia", "South Carolina"},
+                {"Pierre", "South Dakota"},
+                {"Nashville", "Tennessee"},
+                {"Austin", "Texas"},
+                {"Salt Lake City", "Utah"},
+                {"Montpelier", "Vermont"},
+                {"Richmond", "Virginia"},
+                {"Olympia", "Washington"},
+                {"Charleston", "West Virginia"},
+                {"Madison", "Wisconsin"},
+                {"Cheyenne", "Wyoming"},
+        };
+
+        CapitalStateArray csa = new CapitalStateArray(capitalsStates);
+
+        // Print contents of array (line by line)
+        for (String[] capitalState : csa.strArray) {
+            System.out.println(Arrays.toString(capitalState));
+        }
+
+        // Bubble sort
+        csa.bubbleSort();
+
+        // Begin guesses from user and output results at the end
+        GuessGame gg = new GuessGame(capitalsStates);
+        gg.play();
+
+        // Convert 2D array to hashmap
+
+        // Display hashmap
+
+        // Sort map using with TreeMap class
+
+        // Begin user state/capital response
+
+
+    }
 }
